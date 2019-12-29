85954f636d0c2f27da133e40eea0c76a400c2902
varun.anand <varun.anand@nciportal.com>
2019-12-29 20:26:43 +0530

    changes

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import {Recipe} from '../recipe.model'
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
@@ -8,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 export class RecipeListComponent implements OnInit {
 
   constructor() { }
-
+  recipes:Recipe[] = [
+    new Recipe('test','wow','https://www.gimmesomeoven.com/wp-content/uploads/2014/03/Cajun-Jambalaya-Recipe-with-Andouille-Sausage-Shrimp-and-Chicken-32.jpg')
+  ]
   ngOnInit() {
   }
 
